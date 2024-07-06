0a766be838c12f320aa40b11922a1d1eb4eccc8d
Ramen96 <eric.worsfold@gmail.com>
2024-07-06 19:38:21 -0400

    update homepage

--- app/root.jsx
+++ app/root.jsx
@@ -20,22 +20,36 @@ export default function App() {
         <Links />
       </head>
       <body className={styles.body}>
+        <div className={styles.container}>
           <section>
             <nav>
               <div className={styles.navitem}>
-                <p>Home</p>
+                <p><a href="#">Home</a></p>
               </div>
               <div className={styles.navitem}>
-                <p>Cat Chronicles</p>
+                <p><a href="#">Cat Chronicles</a></p>
               </div>
               <div className={styles.navitem}>
-                <p>Cat info</p>
+                <p><a href="#">Learn</a></p>
               </div>
               <div className={styles.navitem}>
-                <p>About</p>
+                <p><a href="#">About</a></p>
               </div>
             </nav>
           </section>
+          <div className={styles.container}>
+            <div className={styles.content}>
+              <p className={styles.p1}>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quo quas excepturi quaerat fuga eum libero reprehenderit exercitationem culpa temporibus? Cupiditate magni doloremque magnam deleniti consequuntur sequi quod sunt necessitatibus nulla.</p>
+              <p className={styles.p1}>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quo quas excepturi quaerat fuga eum libero reprehenderit exercitationem culpa temporibus? Cupiditate magni doloremque magnam deleniti consequuntur sequi quod sunt necessitatibus nulla.</p>
+              <p className={styles.p1}>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quo quas excepturi quaerat fuga eum libero reprehenderit exercitationem culpa temporibus? Cupiditate magni doloremque magnam deleniti consequuntur sequi quod sunt necessitatibus nulla.</p>
+              <p className={styles.p1}>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quo quas excepturi quaerat fuga eum libero reprehenderit exercitationem culpa temporibus? Cupiditate magni doloremque magnam deleniti consequuntur sequi quod sunt necessitatibus nulla.</p>
+              <p className={styles.p1}>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quo quas excepturi quaerat fuga eum libero reprehenderit exercitationem culpa temporibus? Cupiditate magni doloremque magnam deleniti consequuntur sequi quod sunt necessitatibus nulla.</p>
+              <p className={styles.p1}>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quo quas excepturi quaerat fuga eum libero reprehenderit exercitationem culpa temporibus? Cupiditate magni doloremque magnam deleniti consequuntur sequi quod sunt necessitatibus nulla.</p>
+              <p className={styles.p1}>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quo quas excepturi quaerat fuga eum libero reprehenderit exercitationem culpa temporibus? Cupiditate magni doloremque magnam deleniti consequuntur sequi quod sunt necessitatibus nulla.</p>
+              <p className={styles.p1}>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quo quas excepturi quaerat fuga eum libero reprehenderit exercitationem culpa temporibus? Cupiditate magni doloremque magnam deleniti consequuntur sequi quod sunt necessitatibus nulla.</p>
+            </div>
+          </div>
+        </div>
         <Outlet />
 
         <Scripts />
