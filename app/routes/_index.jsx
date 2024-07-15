4cfccc23855e88617bf88b5631ba02d1c7a33712
Ramen96 <eric.worsfold@gmail.com>
2024-07-15 18:30:22 -0400

    added background image

--- app/routes/_index.jsx
+++ app/routes/_index.jsx
@@ -2,9 +2,10 @@ import { gsap } from "gsap/dist/gsap";
 import { TextPlugin } from "gsap/dist/TextPlugin";
 import { useRef } from "react";
 import { useGSAP } from "@gsap/react";
+
 import styles from "../root.module.css";
 import chani1 from "../assets/chani2.png";
-
+import space from "../assets/space.jpg";
 import Nav from "../components/nav/nav";
 
 gsap.registerPlugin(TextPlugin);
@@ -22,11 +23,12 @@ export default function Index() {
 
   return(
     <div className={styles.container}>
+      <img className={styles.space} src={space} alt="space" />
       <Nav />
       <div className={styles.container}>
           <div className={styles.content}>
             <p ref={animatedText} className={styles.p1}></p>
-            <img src={chani1} alt="cat" />
+            <img className={styles.chani1} src={chani1} alt="cat" />
           </div>
         </div>
     </div>
