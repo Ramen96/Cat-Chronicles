a76b4c1503c4b5688e141b2d08eb59b9a177b64f
Ramen96 <eric.worsfold@gmail.com>
2024-07-17 19:07:29 -0400

    updated home page

--- app/routes/_index.jsx
+++ app/routes/_index.jsx
@@ -4,8 +4,8 @@ import { useRef } from "react";
 import { useGSAP } from "@gsap/react";
 
 import styles from "../root.module.css";
-import chani1 from "../assets/chani2.png";
-import space from "../assets/space.avif";
+// import chani1 from "../assets/chani2.png";
+// import space from "../assets/space.avif";
 import Nav from "../components/nav/nav";
 
 gsap.registerPlugin(TextPlugin);
@@ -14,8 +14,8 @@ export default function Index() {
   const animatedText = useRef();
   useGSAP(() => {
     gsap.to(animatedText.current, {
-    duration: 10,
-    text: "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Veniam dolorum sapiente quisquam temporibus, provident culpa maxime a, architecto omnis ad eos animi magni harum voluptatem doloremque impedit quae nam ipsum.",
+    duration: 3,
+    text: "Chani's Cosmic Chronicles: A Tail of Feline Fun in the Final Frontier!",
     delay: 0.5,
     ease: "none"
    });
