5b751eea006d272d9b51276872d27ac87530f46e
Ramen96 <eric.worsfold@gmail.com>
2024-07-17 20:10:50 -0400

    updated home page

--- app/routes/_index.jsx
+++ app/routes/_index.jsx
@@ -30,6 +30,9 @@ export default function Index() {
             <p ref={animatedText} className={styles.textA}></p>
             <div className={styles.containerB}>
               <div className={styles.chani1}></div>
+              <div className={styles.containerC}>
+                <p className={styles.textB}>Welcome to the cosmic adventure of Chani, the space-faring feline who's here to show you that life is better with a dash of feline flair! With paws that have walked the stars and a purr that can soothe even the deepest of black holes, Chani invites you to explore the quirky, the fun, and the absolutely pawsome side of life. Whether you're a cat lover, a space enthusiast, or just someone looking for a good laugh, Chani's got you covered. So buckle up, grab your favorite catnip, and get ready to blast off into a world where every day is a new adventure and every meow is a masterpiece!</p>
+              </div>
             </div>
           </div> 
         </main>
