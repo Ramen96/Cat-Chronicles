44cdee90b378b4474f71ad1efeb6e7019fc84aa1
Ramen96 <eric.worsfold@gmail.com>
2024-07-20 20:42:31 -0400

    switched from css modules to just css

--- app/routes/_index.jsx
+++ app/routes/_index.jsx
@@ -6,6 +6,12 @@ import { useGSAP } from "@gsap/react";
 import styles from "../root.module.css";
 import Nav from "../components/nav/nav";
 
+import "../../css/container.css";
+import "../../css/card.css";
+import "../../css/text.css";
+import "../../css/background.css";
+import "../../css/assets.css";
+
 gsap.registerPlugin(TextPlugin);
 
 export default function Index() {
@@ -20,34 +26,34 @@ export default function Index() {
   })
 
   return(
-    <div className={styles.bgImg}>
+    <div className="bgImg">
       <Nav />
-      <div className={styles.containerA} >
+      <div className="containerA" >
         <main className={styles.main}>
-          <div className={styles.containerB}>
-            <p ref={animatedText} className={styles.textA}></p>
-            <div className={styles.chani1}></div>
-            <div className={styles.containerC}>
-              <p className={styles.textB}>Welcome to the cosmic adventure of Chani, the space-faring feline who's here to show you that life is better with a dash of feline flair!</p>
-              <p className={styles.textB}>With paws that have walked the stars and a purr that can soothe even the deepest of black holes, Chani invites you to explore the quirky, the fun, and the absolutely pawsome side of life.</p>
-              <p className={styles.textB}>Whether you're a cat lover, a space enthusiast, or just someone looking for a good laugh, Chani's got you covered. So buckle up, grab your favorite catnip, and get ready to blast off into a world where every day is a new adventure and every meow is a masterpiece!</p> 
-            </div>
-            <div className={styles.containerB}>
-              <div className={styles.cardA}>
-                <div className={styles.cardAConA}>
-                  <div className={styles.cardAsubA}>
-                    <div className={styles.cardAsubB}>
-
-                    </div>
-                    <div className={styles.cardAsubB}>
-
-                    </div>
-                  </div>
-                </div>
-              </div>
+          <div className="containerB">
+            <p ref={animatedText} className="textA"></p>
+            <div className="chani1"></div>
+            <div className="containerC">
+              <p className="textB">Welcome to the cosmic adventure of Chani, the space-faring feline who's here to show you that life is better with a dash of feline flair!</p>
+              <p className="textB">With paws that have walked the stars and a purr that can soothe even the deepest of black holes, Chani invites you to explore the quirky, the fun, and the absolutely pawsome side of life.</p>
+              <p className="textB">Whether you're a cat lover, a space enthusiast, or just someone looking for a good laugh, Chani's got you covered. So buckle up, grab your favorite catnip, and get ready to blast off into a world where every day is a new adventure and every meow is a masterpiece!</p> 
             </div>
           </div> 
         </main>
+      </div>
+      <div className="containerB">
+        <div className="cardA">
+          <div className="cardAConA">
+            <div className="cardAsubA">
+              <div className="cardAsubB">
+
+              </div>
+              <div className="cardAsubB">
+
+              </div>
+            </div>
+          </div>
+        </div>
       </div>
     </div>
   )
