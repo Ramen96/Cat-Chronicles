d1ebdf9914bef1f9f1e5fa0e9fa4b7da2f84e8d8
Ramen96 <eric.worsfold@gmail.com>
2024-07-21 20:59:20 -0400

    bg for about page

--- app/routes/about/route.jsx
+++ app/routes/about/route.jsx
@@ -1,14 +1,31 @@
 import Nav from "../../components/nav/nav";
 import styles from "./about.module.css";
 
+import chaniGrinch from "../assets/ChaniGrinch.jpg";
+import chaniBox from "../assets/chaniBox.jpg";
+
+import "../../root.css";
+import "../../css/container.css";
+import "../../css/card.css";
+import "../../css/text.css";
+import "../../css/background.css";
+import "../../css/assets.css";
+import "../../css/button.css";
+
 export default function About() {
-  return(
+  return (
     <>
       <Nav />
-      <main>
-        
-      </main>
+      <div className="container0">
+        <div className="containerA conAPosition">
+          <main className="main">
+            <div className="containerB bgImg">
+              <div className="test"></div>
+              {/* The height of this container detrmines the height of the bgImg because of the height auto property in the parent container*/}
+            </div>
+          </main>
+        </div>
+      </div>
     </>
-    
-  )
+  );
 }
