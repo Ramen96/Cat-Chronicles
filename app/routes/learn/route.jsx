b2cd0e0b7d40d5775f5305ccd219b7173ff41ab8
Ramen96 <eric.worsfold@gmail.com>
2024-07-23 19:24:24 -0400

    added background to other pages

--- app/routes/learn/route.jsx
+++ app/routes/learn/route.jsx
@@ -1,8 +1,27 @@
 import Nav from "../../components/nav/nav";
 
+import "../../root.css";
+import "../../css/container.css";
+import "../../css/card.css";
+import "../../css/text.css";
+import "../../css/background.css";
+import "../../css/assets.css";
+import "../../css/button.css";
+
 export default function Learn() {
-  return(
-    <Nav />
-    
-  )
+  return (
+    <>
+      <Nav />
+      <div className="container0">
+        <div className="containerA conAPosition">
+          <main className="main">
+            <div className="containerB bgImg">
+              <div className="test"></div>
+              {/* The height of this container detrmines the height of the bgImg because of the height auto property in the parent container*/}
+            </div>
+          </main>
+        </div>
+      </div>
+    </>
+  );
 }
