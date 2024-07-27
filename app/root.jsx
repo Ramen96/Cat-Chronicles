dcc13e6dd52015474881fa87b7a5a690cd540dde
Ramen96 <eric.worsfold@gmail.com>
2024-07-27 12:26:58 -0400

    created working example of an animated cube using threejs and react three/fiber

--- app/root.jsx
+++ app/root.jsx
@@ -1,10 +1,44 @@
 import { Links, Meta, Outlet, Scripts } from "@remix-run/react";
 
+import * as THREE from "three";
+import React, { useRef, useState } from "react";
+import { Canvas, useFrame } from "@react-three/fiber";
 import { gsap } from "gsap/dist/gsap";
 import { TextPlugin } from "gsap/dist/TextPlugin";
+
 import "./root.css";
 
 gsap.registerPlugin(TextPlugin);
+
+// export async function loader() {
+//   return json({
+//     ENV: {
+//       STRIPE_PUBLIC_KEY: process.env.STRIPE_PUBLIC_KEY,
+//       FAUNA_DB_URL: process.env.FAUNA_DB_URL,
+//     },
+//   });
+// }
+
+function Box(props) {
+  const meshRef = useRef();
+  const [hovered, setHovered] = useState(false);
+  const [active, setActive] = useState(false);
+  useFrame((state, delta) => (meshRef.current.rotation.x += delta));
+
+  return (
+    <mesh
+      {...props}
+      ref={meshRef}
+      scale={active ? 1.5 : 1}
+      onClick={(event) => setActive(true)}
+      onPointerOver={(event) => setHovered(true)}
+      onPointerOut={(event) => setHovered(false)}
+    >
+      <boxGeometry args={[1, 1, 1]} />
+      <meshStandardMaterial color={hovered ? "hotpink" : "orange"} />
+    </mesh>
+  );
+}
 
 export default function App() {
   return (
@@ -15,6 +49,23 @@ export default function App() {
         <Links />
       </head>
       <body>
+        <Canvas>
+          <ambientLight intensity={Math.PI / 2} />
+          <spotLight
+            position={[10, 10, 10]}
+            angle={0.15}
+            penumbra={1}
+            decay={0}
+            intensity={Math.PI}
+          />
+          <pointLight
+            position={[-10, -10, -10]}
+            decay={0}
+            intensity={Math.PI}
+          />
+          <Box position={[-1.2, 0, 0]} />
+          <Box position={[1.2, 0, 0]} />
+        </Canvas>
         <Outlet />
 
         <Scripts />
--- app/routes/_index.jsx
+++ app/routes/_index.jsx
@@ -3,9 +3,9 @@ import { TextPlugin } from "gsap/dist/TextPlugin";
 import { useRef } from "react";
 import { useGSAP } from "@gsap/react";
 
-import Nav from "../components/nav/nav";
-import chaniGrinch from "../assets/ChaniGrinch.jpg";
-import chaniBox from "../assets/chaniBox.jpg";
+// import Nav from "../components/nav/nav";
+// import chaniGrinch from "../assets/ChaniGrinch.jpg";
+// import chaniBox from "../assets/chaniBox.jpg";
 
 import "../root.css";
 import "../css/container.css";
@@ -15,106 +15,50 @@ import "../css/background.css";
 import "../css/assets.css";
 import "../css/button.css";
 
-gsap.registerPlugin(TextPlugin);
+// import * as THREE from "three";
+
+// gsap.registerPlugin(TextPlugin);
 
 export default function Index() {
-  const animatedText = useRef();
-  useGSAP(() => {
-    gsap.to(animatedText.current, {
-      duration: 3,
-      text: "Chani's Cosmic Chronicles: A Voyage of Feline Fun in the Final Frontier!",
-      delay: 0.5,
-      ease: "none",
-    });
-  });
-
-  return (
-    <>
-      <Nav />
-      <div className="container0">
-        <div className="containerA conAPosition">
-          <main className="main">
-            <div className="containerB bgImg">
-              <p ref={animatedText} className="textA"></p>
-              <div className="chani1"></div>
-              <div className="containerC">
-                <p className="textB">
-                  Welcome to the cosmic adventure of Chani, the space-faring
-                  feline who's here to show you that life is better with a dash
-                  of feline flair!
-                </p>
-                <p className="textB">
-                  With paws that have walked the stars and a purr that can
-                  soothe even the deepest of black holes, Chani invites you to
-                  explore the quirky, the fun, and the absolutely pawsome side
-                  of life.
-                </p>
-                <p className="textB">
-                  Whether you're a cat lover, a space enthusiast, or just
-                  someone looking for a good laugh, Chani's got you covered. So
-                  buckle up, grab your favorite catnip, and get ready to blast
-                  off into a world where every day is a new adventure and every
-                  meow is a masterpiece!
-                </p>
-              </div>
-            </div>
-            <div className="containerB">
-              <div className="cardA">
-                <div className="cardAConA">
-                  <div className="cardAsubA">
-                    <div className="cardAsubB">
-                      <img
-                        className="chani1"
-                        src={chaniBox}
-                        alt="Chani Box"
-                      ></img>
-                      <a className="a" href="/about">
-                        <button className="button">
-                          <h1 className="textB">Read More</h1>
-                        </button>
-                      </a>
-                    </div>
-                    <div className="cardAsubB">
-                      <h1 className="textB fontBold">Meet Chani,</h1>
-                      <p className="textC">
-                        the resilient feline who found her forever home on the
-                        side of the road. Rescued on September 4th, 2022, Chani
-                        had a rough start with a broken leg and hip. But after
-                        six months of love and care, she made a full recovery
-                        and now enjoys a playful life filled with coding
-                        sessions, video games, and cuddles with her canine
-                        companion, Buddy. Despite her shyness, Chani's
-                        adventurous spirit and affectionate nature make her a
-                        beloved member of the family.
-                      </p>
-                    </div>
-                  </div>
-                </div>
-              </div>
-            </div>
-            <div className="containerB">
-              <div className="cardA">
-                <p className="textB">Placeholder for preview of Learn page</p>
-              </div>
-            </div>
-            <div className="containerB">
-              <div className="cardA">
-                <p className="textB">
-                  Placehoder for sneekpeak/screenshot of game
-                </p>
-              </div>
-            </div>
-            <div className="containerB">
-              <div className="cardA">
-                <p className="textB">
-                  Placeholder for Call to action for users to keep exploring the
-                  site
-                </p>
-              </div>
-            </div>
-          </main>
-        </div>
-      </div>
-    </>
-  );
+  return <h1>Hello World</h1>;
 }
+// export default function Index() {
+//   const scene = new THREE.Scene();
+// const camera = new THREE.PerspectiveCamera(
+//   75,
+//   this.window.innerWidth / this.window.innerHeight,
+//   0.1,
+//   1000
+// );
+
+// const renderer = new THREE.WebGLRenderer();
+// renderer.setSize(window.innerWidth, window.innerHeight);
+// renderer.setAnimationLoop(animate);
+// document.body.appendChild(renderer.domElement);
+
+// const geometry = new THREE.BoxGeometry(1, 1, 1);
+// const material = new THREE.MeshBasicMaterial({ color: 0x00ff00 });
+// const cube = new THREE.Mesh(geometry, material);
+// scene.add(cube);
+
+// camera.position.z = 5;
+
+// function animate() {
+//   cube.rotation.x += 0.01;
+//   cube.rotation.y += 0.01;
+
+//   renderer.render(scene, camera);
+// }
+
+// const animatedText = useRef();
+// useGSAP(() => {
+//   gsap.to(animatedText.current, {
+//     duration: 3,
+//     text: "Chani's Cosmic Chronicles: A Voyage of Feline Fun in the Final Frontier!",
+//     delay: 0.5,
+//     ease: "none",
+//   });
+// });
+
+// return <script></script>;
+// }
