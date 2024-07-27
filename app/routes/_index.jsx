a5ac2e20c9f583a3866fa8c6806599149269645a
Ramen96 <eric.worsfold@gmail.com>
2024-07-27 12:35:19 -0400

    removed unused code

--- app/routes/_index.jsx
+++ app/routes/_index.jsx
@@ -1,6 +1,9 @@
 import { gsap } from "gsap/dist/gsap";
 import { TextPlugin } from "gsap/dist/TextPlugin";
 import { useGSAP } from "@gsap/react";
+import * as THREE from "three";
+import React, { useRef, useState } from "react";
+import { Canvas, useFrame } from "@react-three/fiber";
 
 // import Nav from "../components/nav/nav";
 // import chaniGrinch from "../assets/ChaniGrinch.jpg";
@@ -13,18 +16,6 @@ import "../css/text.css";
 import "../css/background.css";
 import "../css/assets.css";
 import "../css/button.css";
-
-import * as THREE from "three";
-import React, { useRef, useState } from "react";
-import { Canvas, useFrame } from "@react-three/fiber";
-// export async function loader() {
-//   return json({
-//     ENV: {
-//       STRIPE_PUBLIC_KEY: process.env.STRIPE_PUBLIC_KEY,
-//       FAUNA_DB_URL: process.env.FAUNA_DB_URL,
-//     },
-//   });
-// }
 
 function Box(props) {
   const meshRef = useRef();
@@ -64,48 +55,3 @@ export default function Index() {
     </Canvas>
   );
 }
-
-// import * as THREE from "three";
-
-// gsap.registerPlugin(TextPlugin);
-
-// export default function Index() {
-//   const scene = new THREE.Scene();
-// const camera = new THREE.PerspectiveCamera(
-//   75,
-//   this.window.innerWidth / this.window.innerHeight,
-//   0.1,
-//   1000
-// );
-
-// const renderer = new THREE.WebGLRenderer();
-// renderer.setSize(window.innerWidth, window.innerHeight);
-// renderer.setAnimationLoop(animate);
-// document.body.appendChild(renderer.domElement);
-
-// const geometry = new THREE.BoxGeometry(1, 1, 1);
-// const material = new THREE.MeshBasicMaterial({ color: 0x00ff00 });
-// const cube = new THREE.Mesh(geometry, material);
-// scene.add(cube);
-
-// camera.position.z = 5;
-
-// function animate() {
-//   cube.rotation.x += 0.01;
-//   cube.rotation.y += 0.01;
-
-//   renderer.render(scene, camera);
-// }
-
-// const animatedText = useRef();
-// useGSAP(() => {
-//   gsap.to(animatedText.current, {
-//     duration: 3,
-//     text: "Chani's Cosmic Chronicles: A Voyage of Feline Fun in the Final Frontier!",
-//     delay: 0.5,
-//     ease: "none",
-//   });
-// });
-
-// return <script></script>;
-// }
